c6d60777421df19c5e863b33705bec5311628b6b
AlexandraMGheorghe <77536806+AlexandraMGheorghe@users.noreply.github.com>
2023-06-04 12:34:23 +0300

    Delete src/Homework25 directory

--- src/Homework25/Demo.jsx
+++
@@ -1,6 +0,0 @@
-const container = (
-    <div id="container" className="containerClass">
-        <img src= "http://www.devmind.ro/static/img/logo.png"/>
-        <h3>Hello, World!</h3>
-    </div>
-);
